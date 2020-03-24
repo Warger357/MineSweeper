1716d4eb209ad7aac5255108d2587d369f47f354
Warger357 <misterdamian@wp.pl>
2020-03-24 15:09:30 +0100

    1

---
+++ MineSweeperr/Assets/Scripts/Element.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Element : MonoBehaviour
+{
+    public bool mine;
+    public Sprite[] emptyTextures;
+    public Sprite mineTexture;
+
+    void Start()
+    {
+        mine = Random.value < 0.15;
+
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        grid.elements[x, y] = this;
+
+
+    }
+
+    public void loadTexture(int adjacentCount)
+    {
+        if (mine)
+        {
+            GetComponent<SpriteRenderer>().sprite = mineTexture;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+        }
+    }
+
+    public bool isCovered()
+    {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "default";
+    }
+
+    public void OnMouseUpAsButton()
+    {
+        if (GetComponent<SpriteRenderer>().sprite != emptyTextures[10])
+        {
+            if (mine)
+            {
+                grid.bumBombaKebaboCiaparito();
+            }
+            else
+            {
+
+                int x = (int)transform.position.x;
+                int y = (int)transform.position.y;
+                grid.showNeighbour(x, y, new bool[grid.w, grid.h]);
+                if (grid.isFinished())
+                {
+                    print("you win!");
+                }
+            }
+        }
+    }
+    
+    public void onMouseUp()
+    {
+        
+    }
+    public void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+            if (GetComponent<SpriteRenderer>().sprite == emptyTextures[10])
+            {
+                GetComponent<SpriteRenderer>().sprite = emptyTextures[9]; print("zmień się kurwa");
+                return;
+            }
+            if (GetComponent<SpriteRenderer>().sprite == emptyTextures[9])
+            {
+                GetComponent<SpriteRenderer>().sprite = emptyTextures[10];
+                return;
+            }
+
+        }
+    }
+}
---
+++ MineSweeperr/Assets/Scripts/grid.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grid : MonoBehaviour
+{
+    public static int w = 11; //width
+    public static int h = 14; //height
+    public static Element[,] elements = new Element[w, h];
+
+    public static void bumBombaKebaboCiaparito()
+    {
+        foreach (Element elem in elements)
+        {
+            if (elem.mine)
+                elem.loadTexture(0);
+        }
+    }
+    public static bool mineAt(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < w && y < h)
+        {
+            return elements[x, y].mine;
+        }
+        return false;
+    }
+    public static int minesCount(int x, int y)
+    {
+        int count = 0;
+        if (mineAt(x - 1, y + 1)) count++; //TL
+        if (mineAt(x, y + 1)) count++; //T
+        if (mineAt(x + 1, y + 1)) count++; //TR
+
+        if (mineAt(x - 1, y)) count++; //L
+        if (mineAt(x, y)) count++; //M
+        if (mineAt(x + 1, y)) count++; //R
+
+
+        if (mineAt(x - 1, y - 1)) count++; //BL
+        if (mineAt(x, y - 1)) count++; // B
+        if (mineAt(x + 1, y - 1)) count++; //BR
+
+        return count;
+    }
+    public static void showNeighbour(int x, int y, bool[,] visited)
+    {
+        if (x >= 0 && y >= 0 && x < w && y < h)
+        {
+            if (visited[x, y])
+                return;
+
+            elements[x, y].loadTexture(minesCount(x, y));
+            if (minesCount(x, y) > 0)
+                return;
+
+            visited[x, y] = true;
+            showNeighbour(x - 1, y + 1, visited);
+            showNeighbour(x, y + 1, visited);
+            showNeighbour(x + 1, y + 1, visited);
+
+            showNeighbour(x - 1, y, visited);
+            showNeighbour(x, y, visited);
+            showNeighbour(x + 1, y, visited);
+
+            showNeighbour(x - 1, y - 1, visited);
+            showNeighbour(x, y - 1, visited);
+            showNeighbour(x + 1, y - 1, visited);
+        }
+    }
+    public static bool isFinished()
+    {
+        foreach(Element elem in elements)
+        {
+            if(elem.isCovered() && !elem.mine)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
